0b03362e5eda73699490db71dbc8d0e17eebec02
Giousa <65489469@qq.com>
2022-05-04 15:04:28 +0800

    first commit

---
+++ Consumer/src/main/java/com/books/dubbo/demo/api/GreetingServiceMock.java
@@ -0,0 +1,14 @@
+package com.books.dubbo.demo.api;
+
+public class GreetingServiceMock implements GreetingService{
+
+	@Override
+	public String sayHello(String name) {
+		return "mock value";
+	}
+
+	@Override
+	public Result<String> testGeneric(PoJo poJo) {
+		return null;
+	}
+}
